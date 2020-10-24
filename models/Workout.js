b8934abe5d1ac01037a6f50b61bfc372f9784675
Francis Albuna <falbuna1@gmail.com>
2020-10-24 14:31:50 -0700

    Created the models for the seed.js file.

---
+++ models/Workout.js
@@ -0,0 +1,17 @@
+let mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    date: { type: Date, default: Date.now },
+    exercises: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
