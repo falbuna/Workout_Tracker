3583afbea6495bf6ebfa8ea516c7005e86dd6f47
Francis Albuna <falbuna1@gmail.com>
2020-10-31 09:46:36 -0700

    Added comments to api-routes and html-routes.js file. Removed morgan from server.js file.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app){
-    
+    // This is for the get request the information form the workouts collection in the workout database.
     app.get("/api/workouts", function(req, res) {
         db.Workout.find({})
         .then(dbWorkout => {
@@ -9,7 +9,7 @@ module.exports = function(app){
             console.log(dbWorkout)
         })
     });
-
+    // This is for the post request to create a new workout in the workouts collection of the workout database.
     app.post("/api/workouts", ({ body }, res) => {
         db.Workout.create(body)
         .then(dbWorkout => {
@@ -20,7 +20,7 @@ module.exports = function(app){
             res.status(400).json(err);
         })
     });
-
+    // This is for the put request to push the exercises to the workouts collection in the workout database.
     app.put("/api/workouts/:id", (req, res) => {
         db.Workout.findByIdAndUpdate( req.params.id, { $push: { exercises: req.body } })
         .then(dbWorkout => {
@@ -31,7 +31,7 @@ module.exports = function(app){
             res.status(400).json(err);
         });
     });
-
+    // This is for the get request to get the workout data for the stats collection.
     app.get("/api/workouts/range", function(req, res){
         db.Workout.find({})
         .then(dbWorkout => {
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,15 @@
 var path = require("path");
 
 module.exports = function(app){
-
+    // This will send the user to main index page.
     app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"))
     });
-
+    // This will send the user to the exercise page.
     app.get("/exercise", function(req, res){
         res.sendFile(path.join(__dirname, "../public/exercise.html"))
     });
-
+    // This will send the user to the stats page.
     app.get("/stats", function(req, res){
         res.sendFile(path.join(__dirname, "../public/stats.html"))
     })
